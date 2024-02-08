72d7f09d8ffae84d4e21ad250d4180551efde712
wellifabio <wellifabio@hotmail.com>
2024-02-08 16:05:04 -0300

    aula03

--- ds/2des/02-pwfe/aula03/README.md
+++ ds/2des/02-pwfe/aula03/README.md
@@ -5,6 +5,20 @@
 - Tabelas editaveis
 
 ## Exemplo
-Em continuidade ao sistema Fullstak criado nas aulas3 de Back-End, **CRUD** completo adicionando as funcionalidades UPDATE e DELETE implementaremos estas funcionalidades, porém em uma página só e consumindo corretamente a API.
+Em continuidade ao sistema Fullstak criado nas aulas3 de Back-End, **CRUD** completo adicionando as funcionalidades UPDATE e DELETE implementaremos estas funcionalidades, porém em **uma página só** e consumindo corretamente a API.
 
 ## Telas
+![Tela01](./tela01.png)
+![Tela02](./tela02.png)
+![Tela03](./tela03.png)
+
+## Aplicamos técnicas de UI / UX:
+- UI User interface - HTML + CSS para estilizar a página
+    - Responsividade
+    - Dimenções responsivas
+    - Acessibilidade - Contraste
+- UX User experience
+    - HTML5 no formulário, campos de data
+    - Modais
+    - Funcionalidades em uma só página
+    - Modal para mensagens de erro e confirmação
--- ds/2des/02-pwfe/aula03/lojinha/front/script.js
+++ ds/2des/02-pwfe/aula03/lojinha/front/script.js
@@ -1,3 +1,4 @@
+//Variáveis e constantes(objetos) globais
 const uri = "http://localhost:3000/clientes";
 const clientes = [];
 const msgs = document.getElementById('msgs');
@@ -17,6 +18,7 @@ function loadClientes() {
         });
 }
 
+//CRUD - Read - Renderizar os dados obtidos em uma tabela
 function preencherTabela() {
     clientes.forEach(cli => {
         dados.innerHTML += `
@@ -25,15 +27,17 @@ function preencherTabela() {
                     <td>${cli.cpf}</td>
                     <td>${cli.nome} ${cli.sobrenome}</td>
                     <td>${cli.nascimento.toString().split("T")[0]}</td>
+                    <td>${calcIdade(cli.nascimento)}</td>
                     <td>
                         <button onclick="del(${cli.id})"> - </button>
-                        <button> * </button>
+                        <button onclick="edit(this)"> * </button>
                     </td>
                 </tr>
             `;
     });
 }
 
+//CRUD - Create
 criar.addEventListener('submit', e => {
     e.preventDefault();
     const data = {
@@ -56,22 +60,92 @@ criar.addEventListener('submit', e => {
                 dados.innerHTML = "";
                 preencherTabela();
                 cadastro.classList.add('oculto');
+                criar.reset();
             } else {
                 cadastro.classList.add('oculto');
-                msgs.classList.remove('oculto');
-                document.querySelector('#msg').innerHTML = res.sqlMessage;
+                mensagens(res.sqlMessage, 'Erro ao cadastrar cliente!');
             }
         });
 });
 
+//CRUD - Update
+function update(btn) {
+    let linha = btn.parentNode.parentNode;
+    let celulas = linha.cells;
+    let id = celulas[0].innerHTML;
+    let data = {
+        cpf: celulas[1].innerHTML,
+        nome: celulas[2].innerHTML.split(" ")[0],
+        sobrenome: celulas[2].innerHTML.split(" ")[1],
+        nascimento: celulas[3].innerHTML
+    };
+    fetch(uri + '/' + id, {
+        method: 'PUT',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+    })
+        .then(res => res.json())
+        .then(res => {
+            if (res.sqlMessage == undefined) {
+                celulas[1].removeAttribute('contenteditable');
+                celulas[2].removeAttribute('contenteditable');
+                celulas[3].removeAttribute('contenteditable');
+                btn.innerHTML = '*';
+                btn.setAttribute('onclick', 'edit(this)');
+            } else {
+                mensagens(res.sqlMessage, 'Erro ao atualizar cliente!');
+            }
+        });
+}
+
+//CRUD - Delete
 function del(id) {
+    mensagens('Deseja realmente excluir o cliente id = ' + id + '?', 'Excluir cliente', id);
+}
+
+//Confirma exclusão
+function confirmar(id) {
     fetch(uri + '/' + id, {
         method: 'DELETE'
     })
         .then(res => res.json())
         .then(res => {
-            clientes.splice(clientes.findIndex(cli => cli.id === id), 1);
-            dados.innerHTML = "";
-            preencherTabela();
+            window.location.reload();
         });
+}
+
+//Tornar as células da linha tabela editáveis
+function edit(btn) {
+    let linha = btn.parentNode.parentNode;
+    let celulas = linha.cells;
+    for (let i = 1; i < celulas.length - 1; i++) {
+        celulas[i].setAttribute('contenteditable', 'true');
+    }
+    btn.innerHTML = '✔';
+    btn.setAttribute('onclick', 'update(this)');
+}
+
+//Mostrar mensagens do sistema em um modal
+function mensagens(msg, titulo, confirma) {
+    msgs.classList.remove('oculto');
+    document.querySelector('#errTit').innerHTML = titulo;
+    document.querySelector('#msg').innerHTML = msg;
+    if (confirma != undefined) {
+        document.querySelector('#confirma').classList.remove('oculto');
+        document.querySelector('#confirma').setAttribute("onclick", `confirmar(${confirma})`);
+    }
+}
+
+//Calcular idade a partir da data de nascimento
+function calcIdade(nascimento) {
+    let data = new Date(nascimento);
+    let hoje = new Date();
+    let idade = hoje.getFullYear() - data.getFullYear();
+    let mes = hoje.getMonth() - data.getMonth();
+    if (mes < 0 || (mes === 0 && hoje.getDate() < data.getDate())) {
+        idade--;
+    }
+    return idade;
 }
