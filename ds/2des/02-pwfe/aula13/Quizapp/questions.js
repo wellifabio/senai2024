c16b335ad99f0ad05eff47a8cac2ea5eedb508b8
wellifabio <robsonbacchindesouza@gmail.com>
2024-05-13 09:38:33 -0300

    Update questions.js

---
+++ ds/2des/02-pwfe/aula13/Quizapp/questions.js
@@ -0,0 +1,39 @@
+export default [
+    {
+        question: "Quanto é 1 + 2?",
+        answers: [
+            {option: "3", correct:true},
+            {option: "4", correct:false},
+            {option: "5", correct:false},
+        ],
+    },
+
+    {
+        question: "Quanto é 5 x 2?",
+        answers: [
+            {option: "9", correct:false},
+            {option: "10", correct:true},
+            {option: "11", correct:false},
+        ],
+    },
+    
+    {
+        question: "Quanto é 10 x 8?",
+        answers: [
+            {option: "800", correct:false},
+            {option: "60", correct:false},
+            {option: "80", correct:true},
+        ],
+        
+    },
+
+    {
+        question: "Quanto é 7 x 7?",
+        answers: [
+            {option: "49", correct:true},
+            {option: "50", correct:false},
+            {option: "51", correct:false},
+        ],
+        
+    },
+];
