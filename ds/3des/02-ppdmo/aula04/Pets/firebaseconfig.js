5731ae8be7ca4d86f432bd455215aa221d349eae
Robson Souza <robsonbacchindesouza@gmail.com>
2024-08-30 07:53:41 -0300

    up

---
+++ ds/3des/02-ppdmo/aula04/Pets/firebaseconfig.js
@@ -0,0 +1,23 @@
+import {initializeApp} from 'firebase/app';
+import {getFirestore} from 'firebase/firestore';
+import {getAuth} from 'firebase/auth';
+import { getStorage } from 'firebase/storage';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCgYr2g3NyP0V7LtSS8tSuzDmxWA_FNhFE",
+    authDomain: "pets-2024.firebaseapp.com",
+    projectId: "pets-2024",
+    storageBucket: "pets-2024.appspot.com",
+    messagingSenderId: "346960568568",
+    appId: "1:346960568568:web:eaffa20f6332b90fba9b0d"
+  };
+  
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+
+  const db = getFirestore(app);
+
+  const auth = getAuth(app);
+  export const storage = getStorage(app);
+
+    export {db, auth};
